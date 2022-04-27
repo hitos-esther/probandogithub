d49e6f8f524a6e09b79545510315d62c1a801bdf
estherhitos <esther.hitos@iescampanillas.com>
2022-04-27 19:40:55 +0200

    primer commit proyecto git

---
+++ src/Controlador/Principal.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Controlador;
+
+/**
+ * @author ESTHER
+ *
+ */
+public class Principal {
+
+		static String nombre="Esther";
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi nombre es:"+nombre);
+	}
+
+}
