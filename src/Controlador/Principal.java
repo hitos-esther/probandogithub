0557245c77a12a79662e550193d7d3a63b18296e
estherhitos <esther.hitos@iescampanillas.com>
2022-04-27 19:41:56 +0200

    Segundo commit con variable primer apellido

--- src/Controlador/Principal.java
+++ src/Controlador/Principal.java
@@ -10,12 +10,14 @@ package Controlador;
 public class Principal {
 
 		static String nombre="Esther";
+		static String apellido="Hitos";
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mi nombre es:"+nombre);
+		System.out.println("El primer Apellido es: "+apellido);
 	}
 
 }
